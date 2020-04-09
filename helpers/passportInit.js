82e2ab20374b0ab2d7a56bdd67f1a3bf5488426f
Konstantin <stoyanov.k1992@gmail.com>
2020-04-09 17:58:41 +0300

    add bodyParser cors

--- helpers/passportInit.js
+++ helpers/passportInit.js
@@ -5,12 +5,12 @@ const bcrypt = require("bcrypt");
 async function init(email, password, done) {
   try {
     const user = await User.findOne({ where: { email } });
+    if (!user) return done(null, false, { message: "user is not exist" });
 
     const match = await bcrypt.compare(password, user.user_password);
 
-    if (!user) return done(null, false, { message: "user is not exist" });
     if (!match) return done(null, false, { message: "wrong password" });
-    return done(null, { user: user.user_name, email: user.email });
+    return done(null, user);
   } catch (e) {
     done(e);
   }
@@ -18,10 +18,14 @@ async function init(email, password, done) {
 
 function passportInit(passport) {
   passport.use(new LocalStrategy({ usernameField: "email" }, init));
+
   passport.serializeUser((user, done) => {
+    // console.log(user);
     done(null, user);
   });
+
   passport.deserializeUser((user, done) => {
+    console.log(user);
     done(null, user);
   });
 }
--- index.js
+++ index.js
@@ -6,30 +6,41 @@ const app = express();
 const sequelize = require("./database");
 const api = require("./api/routes");
 const passport = require("passport");
-const passportInit = require("./helpers/passportInit")
+const passportInit = require("./helpers/passportInit");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
 app.use(
   session({
     store: new FileStore(),
     secret: process.env.SECRET_WORD,
     resave: false,
     saveUninitialized: false,
-    cookie: { maxAge: 60 * 60 * 24 }
+    cookie: { maxAge: 60 * 60 * 24 },
   })
 );
 app.use(passport.initialize());
 app.use(passport.session());
 
-passportInit(passport)
+passportInit(passport);
 
 api(app);
 
 async function start() {
   try {
     await sequelize.sync({ force: false });
-    app.listen(3000, function() {
+    app.listen(3000, function () {
       console.log("Example app listening on port 3000!");
     });
   } catch (e) {
