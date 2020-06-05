d6a4bc194f35117ab94cad5ce30579ab8fc5c581
Konstantin <stoyanov.k1992@gmail.com>
2020-06-05 14:12:57 +0300

    add db dump

--- db.config.js
+++ db.config.js
@@ -4,3 +4,4 @@ module.exports = {
   PASSWORD: "0cf739aa",
   DB: "heroku_898f2acf62bda13",
 };
+//mysql://b9f423f8189e40:0cf739aa@us-cdbr-east-05.cleardb.net/heroku_898f2acf62bda13?reconnect=true
--- db.sql
+++ db.sql
@@ -25,17 +25,17 @@ DROP TABLE IF EXISTS `comments`;
 CREATE TABLE `comments` (
   `id` int NOT NULL AUTO_INCREMENT,
   `description` varchar(255) DEFAULT NULL,
-  `date` datetime DEFAULT NULL,
   `createdAt` datetime NOT NULL,
   `updatedAt` datetime NOT NULL,
   `result_id` int DEFAULT NULL,
   `task_id` int DEFAULT NULL,
+  `owner_id` int DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `result_id` (`result_id`),
   KEY `task_id` (`task_id`),
   CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`result_id`) REFERENCES `results` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
   CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`task_id`) REFERENCES `tasks` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -44,6 +44,7 @@ CREATE TABLE `comments` (
 
 LOCK TABLES `comments` WRITE;
 /*!40000 ALTER TABLE `comments` DISABLE KEYS */;
+INSERT INTO `comments` VALUES (1,'','2020-04-26 19:20:19','2020-04-26 19:20:19',NULL,6,1),(2,'bad test','2020-04-26 19:23:23','2020-04-26 19:23:23',NULL,6,1);
 /*!40000 ALTER TABLE `comments` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -64,7 +65,7 @@ CREATE TABLE `projects` (
   PRIMARY KEY (`id`),
   KEY `owner_id` (`owner_id`),
   CONSTRAINT `projects_ibfk_1` FOREIGN KEY (`owner_id`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -73,6 +74,7 @@ CREATE TABLE `projects` (
 
 LOCK TABLES `projects` WRITE;
 /*!40000 ALTER TABLE `projects` DISABLE KEYS */;
+INSERT INTO `projects` VALUES (1,'react project','react','2020-04-21 07:23:33','2020-04-21 07:23:33',1),(2,'react project 2','react','2020-04-21 07:23:46','2020-04-21 07:23:46',1),(3,'react project 3','react','2020-04-21 07:23:49','2020-04-21 07:23:49',1),(4,'vue 2','vue','2020-04-21 07:24:01','2020-04-21 07:24:01',1),(5,'vue 1','vue','2020-04-21 07:24:05','2020-04-21 07:24:05',1),(6,'vue 3','vue','2020-04-21 07:24:08','2020-04-21 07:24:08',1),(7,'old project 1','angular 2','2020-04-21 07:25:29','2020-04-21 07:25:29',2),(8,'old project 2','angular 2','2020-04-21 07:25:34','2020-04-21 07:25:34',2),(9,'old project 3','angular 2','2020-04-21 07:25:37','2020-04-21 07:25:37',2),(10,'old project 4','angular 7','2020-04-23 11:16:18','2020-04-23 11:16:18',1);
 /*!40000 ALTER TABLE `projects` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -85,8 +87,7 @@ DROP TABLE IF EXISTS `results`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `results` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `result` varchar(255) DEFAULT NULL,
-  `date` datetime DEFAULT NULL,
+  `result` tinyint(1) DEFAULT NULL,
   `createdAt` datetime NOT NULL,
   `updatedAt` datetime NOT NULL,
   `task_id` int DEFAULT NULL,
@@ -96,7 +97,7 @@ CREATE TABLE `results` (
   KEY `owner_id` (`owner_id`),
   CONSTRAINT `results_ibfk_1` FOREIGN KEY (`task_id`) REFERENCES `tasks` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
   CONSTRAINT `results_ibfk_2` FOREIGN KEY (`owner_id`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -105,7 +106,32 @@ CREATE TABLE `results` (
 
 LOCK TABLES `results` WRITE;
 /*!40000 ALTER TABLE `results` DISABLE KEYS */;
+INSERT INTO `results` VALUES (1,1,'2020-05-02 17:48:57','2020-05-02 17:48:57',6,1);
 /*!40000 ALTER TABLE `results` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `SequelizeMeta`
+--
+
+DROP TABLE IF EXISTS `SequelizeMeta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `SequelizeMeta` (
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`name`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `SequelizeMeta`
+--
+
+LOCK TABLES `SequelizeMeta` WRITE;
+/*!40000 ALTER TABLE `SequelizeMeta` DISABLE KEYS */;
+INSERT INTO `SequelizeMeta` VALUES ('20200424055104-add_desc_in_tasks.js'),('20200426185736-add_comments_column_owner_id.js'),('20200429184135-change_result_type.js');
+/*!40000 ALTER TABLE `SequelizeMeta` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -123,6 +149,7 @@ CREATE TABLE `tasks` (
   `task_id` int DEFAULT NULL,
   `owner_id` int DEFAULT NULL,
   `project_id` int DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `task_id` (`task_id`),
   KEY `owner_id` (`owner_id`),
@@ -130,7 +157,7 @@ CREATE TABLE `tasks` (
   CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`task_id`) REFERENCES `tasks` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
   CONSTRAINT `tasks_ibfk_2` FOREIGN KEY (`owner_id`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
   CONSTRAINT `tasks_ibfk_3` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -139,6 +166,7 @@ CREATE TABLE `tasks` (
 
 LOCK TABLES `tasks` WRITE;
 /*!40000 ALTER TABLE `tasks` DISABLE KEYS */;
+INSERT INTO `tasks` VALUES (6,'test ','2020-04-26 06:36:18','2020-04-26 06:36:18',NULL,1,1,'make great test');
 /*!40000 ALTER TABLE `tasks` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -152,15 +180,13 @@ DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `id` int NOT NULL AUTO_INCREMENT,
   `user_name` varchar(255) DEFAULT NULL,
-  `user_password` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
   `admin` tinyint(1) NOT NULL DEFAULT '0',
   `email` varchar(255) DEFAULT NULL,
-  `temporary_token` varchar(255) DEFAULT NULL,
-  `life_time_token` varchar(255) DEFAULT NULL,
   `createdAt` datetime NOT NULL,
   `updatedAt` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -169,7 +195,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (5,'MasterGennadiy','$2b$10$wCGXIeV3LBAqAKrVho5Bgua/d/CnB0C0FAEj53vi/ktEHv//ZY2bG',0,'lolkek@gmail.com',NULL,NULL,'2020-03-30 19:35:41','2020-03-30 19:35:41'),(7,'MasterFlomaster','$2b$10$wA46jCCj4y6mURkMKEYFreixJ8Cyr3S7hmfdj5ZzSiDT3UXCS7Enq',0,'masterflomaster@gmail.com',NULL,NULL,'2020-03-30 19:49:23','2020-03-30 19:49:23');
+INSERT INTO `users` VALUES (1,'admin','$2b$10$z4sq0JMoZUeaKi0TexCd2eSjjajpLsrTliBI6v2i0ZD9w0YyZ39Vu',1,'stoyanov.k1992@gmail.com','2020-04-21 07:07:37','2020-04-21 07:07:37'),(2,'user','$2b$10$5jI2ZoVaVY3o8wuRZo1ExeP5cVt2FfcO1g569v.5dUEwMvcRg3F0q',0,'123stoyanov.k1992@gmail.ru','2020-04-21 07:24:42','2020-04-21 07:24:42');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -191,7 +217,7 @@ CREATE TABLE `workers` (
   KEY `user_id` (`user_id`),
   CONSTRAINT `workers_ibfk_1` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `workers_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -200,6 +226,7 @@ CREATE TABLE `workers` (
 
 LOCK TABLES `workers` WRITE;
 /*!40000 ALTER TABLE `workers` DISABLE KEYS */;
+INSERT INTO `workers` VALUES (1,'2020-04-21 07:23:33','2020-04-21 07:23:33',1,2),(2,'2020-04-21 07:23:46','2020-04-21 07:23:46',2,2),(3,'2020-04-21 07:23:49','2020-04-21 07:23:49',2,1),(4,'2020-04-21 07:24:01','2020-04-21 07:24:01',4,1),(5,'2020-04-21 07:24:05','2020-04-21 07:24:05',5,1),(6,'2020-04-21 07:24:08','2020-04-21 07:24:08',6,1),(7,'2020-04-21 07:25:29','2020-04-21 07:25:29',7,1),(8,'2020-04-21 07:25:34','2020-04-21 07:25:34',8,1),(10,'2020-04-21 07:24:01','2020-04-21 07:24:01',5,2),(11,'2020-05-02 19:02:42','2020-05-02 19:02:42',9,1);
 /*!40000 ALTER TABLE `workers` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -220,4 +247,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-03-30 22:56:39
+-- Dump completed on 2020-06-05 14:09:50
