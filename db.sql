67eabe21b55957d61938cb603b21687a4bff1099
Konstantin <stoyanov.k1992@gmail.com>
2020-06-30 21:19:45 +0300

    change db.sql

--- db.sql
+++ db.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.20, for macos10.15 (x86_64)
 --
--- Host: localhost    Database: testcases
+-- Host: 127.0.0.1    Database: test-case-app
 -- ------------------------------------------------------
--- Server version	8.0.19
+-- Server version	8.0.20
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -24,7 +24,7 @@ DROP TABLE IF EXISTS `comments`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `comments` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `description` varchar(255) DEFAULT NULL,
+  `description` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `createdAt` datetime NOT NULL,
   `updatedAt` datetime NOT NULL,
   `result_id` int DEFAULT NULL,
@@ -58,15 +58,15 @@ DROP TABLE IF EXISTS `projects`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `projects` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `project_name` varchar(255) DEFAULT NULL,
-  `description` text,
+  `project_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci,
   `createdAt` datetime NOT NULL,
   `updatedAt` datetime NOT NULL,
   `owner_id` int DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `owner_id` (`owner_id`),
   CONSTRAINT `projects_ibfk_1` FOREIGN KEY (`owner_id`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -75,7 +75,7 @@ CREATE TABLE `projects` (
 
 LOCK TABLES `projects` WRITE;
 /*!40000 ALTER TABLE `projects` DISABLE KEYS */;
-INSERT INTO `projects` VALUES (3,'3','something very interesting','2020-06-18 18:34:15','2020-06-18 18:34:15',1);
+INSERT INTO `projects` VALUES (3,'React project','<p>something very interestingsnvkdjbnaolvkndflk</p>\n','2020-06-18 18:34:15','2020-06-23 16:28:53',1),(4,'chroma_souls___','<p style=\"text-align:start;\"><span style=\"color: rgb(34,34,34);background-color: rgb(255,255,255);font-size: 16px;font-family: -apple-system, system-ui, \"Segoe UI\", Arial, sans-serif;\">Потратив последние несколько месяцев на разработку приложений на React и библиотек с использованием Typescript, я решил поделиться некоторыми вещами, которые узнал за это время. В этом руководстве я расскажу вам про шаблоны, которые я использую для Typescript и React в 80% случаев.</span><br><br><span style=\"color: rgb(34,34,34);background-color: rgb(255,255,255);font-size: 16px;font-family: -apple-system, system-ui, \"Segoe UI\", Arial, sans-serif;\">Стоит ли изучать Typescript для разработки приложений на React? Стоит, еще как стоит! Для себя я осознал на практике, что строгая типизация приводит к написанию гораздо более надежного кода, быстрой разработке, особенно в крупных проектах. Сначала вы, вероятно, будете разочарованы, но по мере работы вы обнаружите, что хотя бы минимальный шаблон действительно будет очень кстати.</span><br><br><span style=\"color: rgb(34,34,34);background-color: rgb(255,255,255);font-size: 16px;font-family: -apple-system, system-ui, \"Segoe UI\", Arial, sans-serif;\">И если вы застряли на чем-то, помните, что вы всегда можете типизировать что- нибудь как any. Any – ваш новый друг. А теперь перейдем непосредственно к примерам.</span><br><br><span style=\"color: rgb(34,34,34);background-color: rgb(255,255,255);font-size: 24px;font-family: Fira Sans\", sans-serif;\">Ваш базовый компонент react с typescript</span><br><span style=\"color: rgb(34,34,34);background-color: rgb(255,255,255);font-size: 16px;font-family: -apple-system, system-ui, \"Segoe UI\", Arial, sans-serif;\">Как же выглядит стандартный компонент react на typescript? Давайте сравним его с компонентом react в javascript.</span><br></p>\n','2020-06-21 10:58:12','2020-06-21 10:58:12',1);
 /*!40000 ALTER TABLE `projects` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -130,7 +130,7 @@ CREATE TABLE `sequelizemeta` (
 
 LOCK TABLES `sequelizemeta` WRITE;
 /*!40000 ALTER TABLE `sequelizemeta` DISABLE KEYS */;
-INSERT INTO `sequelizemeta` VALUES ('20200424055104-add_desc_in_tasks.js'),('20200426185736-add_comments_column_owner_id.js'),('20200429184135-change_result_type.js'),('20200611141544-remove_subtasks.js'),('20200618064317-add_user_token.js'),('20200618064317-change_user_token.js');
+INSERT INTO `sequelizemeta` VALUES ('20200424055104-add_desc_in_tasks.js'),('20200426185736-add_comments_column_owner_id.js'),('20200429184135-change_result_type.js'),('20200611141544-remove_subtasks.js'),('20200618064317-add_user_token.js'),('20200624055059-create_token_table.js');
 /*!40000 ALTER TABLE `sequelizemeta` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -143,8 +143,8 @@ DROP TABLE IF EXISTS `tasks`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `tasks` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `task_name` varchar(255) DEFAULT NULL,
-  `description` text,
+  `task_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci,
   `createdAt` datetime NOT NULL,
   `updatedAt` datetime NOT NULL,
   `owner_id` int DEFAULT NULL,
@@ -168,6 +168,35 @@ INSERT INTO `tasks` VALUES (6,'6','make great test','2020-06-18 18:35:16','2020-
 UNLOCK TABLES;
 
 --
+-- Table structure for table `tokens`
+--
+
+DROP TABLE IF EXISTS `tokens`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tokens` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `token` varchar(1234) DEFAULT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  `updatedAt` datetime DEFAULT NULL,
+  `owner_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `owner_id` (`owner_id`),
+  CONSTRAINT `tokens_ibfk_1` FOREIGN KEY (`owner_id`) REFERENCES `Users` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tokens`
+--
+
+LOCK TABLES `tokens` WRITE;
+/*!40000 ALTER TABLE `tokens` DISABLE KEYS */;
+INSERT INTO `tokens` VALUES (7,'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxLCJ1c2VyX25hbWUiOiJLb25zdGFudGluIiwicGFzc3dvcmQiOiIkMmIkMTAkT09Yby9zN3ZuZDhWVFRLT3lERGY1T3l0RlAwQ2hYNmI5ZFRlRnBkOUZZZTc4c0pPVGQ0ck8iLCJhZG1pbiI6dHJ1ZSwiZW1haWwiOiJzdG95YW5vdi5rMTk5MkBnbWFpbC5jb20iLCJjcmVhdGVkQXQiOiIyMDIwLTA2LTE4VDE4OjI3OjExLjAwMFoiLCJ1cGRhdGVkQXQiOiIyMDIwLTA2LTIzVDE2OjQxOjE4LjAwMFoiLCJ0b2tlbnMiOlt7ImlkIjo3fV19LCJyZW1lbWJlciI6dHJ1ZSwiaWF0IjoxNTkzNDU0MjAyfQ.-hyIceQ8b8EFJ9cb2X6SqURmf_sCk0L_wee4YNKDR_A','2020-06-28 14:23:21','2020-06-29 18:10:02',1);
+/*!40000 ALTER TABLE `tokens` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `users`
 --
 
@@ -176,11 +205,10 @@ DROP TABLE IF EXISTS `users`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `user_name` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
+  `user_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `admin` tinyint(1) NOT NULL DEFAULT '0',
-  `email` varchar(255) DEFAULT NULL,
-  `token` varchar(1234) DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `createdAt` datetime NOT NULL,
   `updatedAt` datetime NOT NULL,
   PRIMARY KEY (`id`)
@@ -193,7 +221,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'Konstantin','$2b$10$OOXo/s7vnd8VTTKOyDDf5OytFP0ChX6b9dTeFpd9FYe78sJOTd4rO',1,'stoyanov.k1992@gmail.com','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxLCJ1c2VyX25hbWUiOiJLb25zdGFudGluIiwiYWRtaW4iOnRydWUsImVtYWlsIjoic3RveWFub3YuazE5OTJAZ21haWwuY29tIiwiY3JlYXRlZEF0IjoiMjAyMC0wNi0xOFQxODoyNzoxMS4wMDBaIiwidXBkYXRlZEF0IjoiMjAyMC0wNi0xOFQxODoyNzoxMS4wMDBaIn0sInJlbWVtYmVyIjpmYWxzZSwiaWF0IjoxNTkyNTA1MTk2fQ.0b64s6CxxAZcf-w6n4xBs01iRxUy2cAvRmdfCif-LjE','2020-06-18 18:27:11','2020-06-18 18:33:16');
+INSERT INTO `users` VALUES (1,'Konstantin','$2b$10$OOXo/s7vnd8VTTKOyDDf5OytFP0ChX6b9dTeFpd9FYe78sJOTd4rO',1,'stoyanov.k1992@gmail.com','2020-06-18 18:27:11','2020-06-23 16:41:18');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -226,10 +254,6 @@ LOCK TABLES `workers` WRITE;
 /*!40000 ALTER TABLE `workers` DISABLE KEYS */;
 /*!40000 ALTER TABLE `workers` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Dumping events for database 'testcases'
---
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -240,4 +264,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-06-18 22:23:35
+-- Dump completed on 2020-06-30 21:13:15
